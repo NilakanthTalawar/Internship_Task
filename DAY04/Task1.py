6fbe24f9b2a153ccf3c69268e9724d752c19d44c
Nilakanth Talawar <neelakantha2004@gmail.com>
2026-02-07 16:43:12 +0530

    Add error counting function for log file

--- DAY04/Task1.py
+++ DAY04/Task1.py
@@ -9,3 +9,24 @@ file.write("Error: timeout occurred\n")
 file.close()
 
 print("File created")
+
+
+def count_errors(filename):
+    error_lines = []
+    count = 0
+    try:
+        file = open(filename, "r")
+        for line in file:
+            if "error" in line.lower():
+                count = count + 1
+                error_lines.append(line)
+        file.close()
+        log = open("error_log.txt", "w")
+        for e in error_lines:
+            log.write(e)
+        log.close()
+        return count
+    except:
+        return "File not found"
+
+print("Total Errors:", count_errors("log.txt"))
